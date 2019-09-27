939c3b21732c0eb86038eff2808c467b95f1d9bc
vuxuanthanh <vuxuanthanh2k@gmail.com>
2019-09-27 19:47:13 +0700

    addfilejava

---
+++ BaiTap/BaiTapJava.java
@@ -0,0 +1,26 @@
+package BaiTap;
+
+import java.util.Scanner;
+// Khai bao mang
+public class BaiTapJava {
+
+    private static int nguyenTo(int n){
+        int dem=0;
+        for(int j=1;j<=n;j++){
+            if(n%j==0)
+                dem++;
+        }
+        if(dem==2)
+            return 2;
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=1;i<=n;i++){
+            if(nguyenTo(i)==2)
+                System.out.println(i);
+        }
+
+    }
+}
